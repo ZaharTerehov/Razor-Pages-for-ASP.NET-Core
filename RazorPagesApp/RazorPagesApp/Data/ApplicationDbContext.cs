a5b1f2808dea4182ba40eb7babd10f7c0d7325e6
z.terehov <zaharkuli67@gmail.com>
2022-11-21 12:08:47 +0300

    added model category and applicationdb context

---
+++ RazorPagesApp/RazorPagesApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPagesApp.Model;
+
+namespace RazorPagesApp.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<Category> Category { get; set; }
+	}
+}
---
+++ RazorPagesApp/RazorPagesApp/Model/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesApp.Model
+{
+	public class Category
+	{
+		[Key]
+		public Guid Id { get; set; }
+
+		[Required(ErrorMessage = "Enter your name!")]
+		[StringLength(30, MinimumLength = 3, ErrorMessage = "It is very short name!")]
+		public string Name { get; set; }
+
+		public int DisplayOrder { get; set; }
+	}
+}
